7c3f57c2529735f3f0347be833f7813b7918a207
Mazhar Naseer <mzrnsr@hotmail.com>
2026-08-07 00:07:26 +0500

    allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,7 +36,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.frontend_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
